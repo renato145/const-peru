18fd78cbc189f4a682149117d1fe5e6efed1b526
Renato <renato145@hotmail.com>
2020-11-21 18:35:23 +1030

    tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const App: React.FC = () => (
-  <div>
-    lalala
+  <div className="h-screen flex justify-center items-center bg-red-400">
+    <p className="text-4xl font-bold">Something should be here later</p>
   </div>
 );
