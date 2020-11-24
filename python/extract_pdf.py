617f59c3c301b28e084b256706f632e9e1a44ee4
Renato <renato145@hotmail.com>
2020-11-24 15:30:24 +1030

    get refs from titles

--- python/extract_pdf.py
+++ python/extract_pdf.py
@@ -147,11 +147,12 @@ def get_all_articles(data):
     if len(errors) > 0: Exception(f'Following articles not found: {errors}')
     return res
 
-def add_references(data, footnotes):
+def add_references(data, footnotes, text_field='text'):
     for d in data:
-        refs = pat_ref.findall(d['text'])
+        refs = pat_ref.findall(d[text_field])
         refs = [{'ref':int(ref), 'text':footnotes[int(ref)]} for ref in refs]
-        d['footnotes'] = refs
+        d['footnotes'] = d.get('footnotes', []) + refs
+        # d['footnotes'] = footnotes
 
 def get_chapter_names(x):
     if 'text' in x['chapters']: return []
@@ -194,9 +195,11 @@ if __name__ == "__main__":
     chapters[-1]['chapters']['text'] = new_end
 
     articles = get_all_articles(chapters)
+    add_references(articles, pdf_footnotes, 'name')
     add_references(articles, pdf_footnotes)
     add_references(end_sections, pdf_footnotes)
     names = [{'i': k, 'name': v['name'], 'chapters': get_chapter_names(chapters[k-1])} for k,v in titles.items()]
+    for title in names: add_references(title['chapters'], pdf_footnotes, 'name')
 
     objs = [names, intro, articles, end_sections]
     names = ['names', 'intro', 'articles', 'end_sections']
